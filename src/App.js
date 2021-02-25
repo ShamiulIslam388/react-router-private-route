6d0d0550a2d49bd652e5e19bd8f4a1c34b247ba5
ShamiulIslam388 <58252309+ShamiulIslam388@users.noreply.github.com>
2021-02-25 19:09:37 +0600

    completing react-router part

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
+import Navigation from "./components/Navigation";
 
-export default function App() {
+const App = () => {
   return (
     <BrowserRouter className="App">
+      <Navigation />
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
       <Route exact path="/contact" component={Contact} />
     </BrowserRouter>
   );
-}
+};
+
+export default App;
